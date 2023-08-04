43c8ab95470d94a14dd1ff63ffe75bba4a58331b
Sergey Cherepanov <cheggaaa@gmail.com>
2023-08-04 12:56:36 +0200

    nodeconf: add consensus type

--- coordinator/rpchandler.go
+++ coordinator/rpchandler.go
@@ -133,6 +133,8 @@ func (r *rpcHandler) NetworkConfiguration(ctx context.Context, req *coordinatorp
 					types = append(types, coordinatorproto.NodeType_FileAPI)
 				case nodeconf.NodeTypeTree:
 					types = append(types, coordinatorproto.NodeType_TreeAPI)
+				case nodeconf.NodeTypeConsensus:
+					types = append(types, coordinatorproto.NodeType_ConsensusAPI)
 				}
 			}
 			nodes = append(nodes, &coordinatorproto.Node{
